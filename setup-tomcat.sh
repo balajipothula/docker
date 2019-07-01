44e6ba11961a180c75a5fa5f096375d017cede35
BALAJI POTHULA <balan.pothula@gmail.com>
2019-07-01 12:23:08 +0530

    Update setup-tomcat.sh

--- setup-tomcat.sh
+++ setup-tomcat.sh
@@ -4,10 +4,10 @@
 # Date        : 31 August 2016,
 # Description : Setup Tomcat8.
 
-wget http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.42/bin/apache-tomcat-8.5.42.tar.gz  && \
-tar xzf apache-tomcat-8.5.42.tar.gz                                                                 && \
-mv apache-tomcat-8.5.42 /root/.tomcat                                                               && \
-rm -rf apache-tomcat-8.5.42.tar.gz                                                                  && \
-find /root/.tomcat -maxdepth 1 -type f -delete                                                      && \
-find /root/.tomcat/bin -maxdepth 1 -name "*.bat" -type f -delete                                    && \
-rm -rf /root/.tomcat/webapps/docs /root/.tomcat/webapps/examples /root/.tomcat/webapps/host-manager
+wget http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.42/bin/apache-tomcat-8.5.42.tar.gz -O $HOME/root/tomcat.tar.gz && \
+tar xzf tomcat.tar.gz                                                                                                          && \
+mv apache-tomcat-8.5.42 $HOME/root/.tomcat                                                                                     && \
+rm -rf apache-tomcat-8.5.42.tar.gz                                                                                             && \
+find $HOME/root/.tomcat -maxdepth 1 -type f -delete                                                                            && \
+find $HOME/root/.tomcat/bin -maxdepth 1 -name "*.bat" -type f -delete                                                          && \
+rm -rf $HOME/root/.tomcat/webapps/docs $HOME/root/.tomcat/webapps/examples $HOME/root/.tomcat/webapps/host-manager

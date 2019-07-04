76e3577adb1a7f48a1ece328f52ec4f287784258
BALAJI POTHULA <balan.pothula@gmail.com>
2019-07-04 16:39:09 +0530

    Update setup-tomcat.sh

--- setup-tomcat.sh
+++ setup-tomcat.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
+FROM alpine:3.9
+RUN apk update && apk upgrade && apk add --no-cache openjdk8-jre tomcat-native && rm -rf /tmp/* /var/tmp/* /var/cache/apk/* /var/cache/distfiles/*
+VOLUME /root/.tomcat
+CMD ["/root/.tomcat/bin/catalina.sh", "run"]
 
-# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
-# Date        : 31 August 2016,
-# Description : Setup Tomcat8.
+docker build -t balajipothula/tomcat:openjdk8-jre .
 
-curl http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.42/bin/apache-tomcat-8.5.42.tar.gz -o $HOME/root/tomcat.tar.gz && \
-tar xzf $HOME/root/tomcat.tar.gz -C $HOME/root/                                                                                && \
-mv $HOME/root/apache-tomcat-8.5.42 $HOME/root/.tomcat                                                                          && \
-rm -rf $HOME/root/tomcat.tar.gz                                                                                                && \
-find $HOME/root/.tomcat -maxdepth 1 -type f -delete                                                                            && \
-find $HOME/root/.tomcat/bin -maxdepth 1 -name "*.bat" -type f -delete                                                          && \
+mkdir root
+
+curl http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.42/bin/apache-tomcat-8.5.42.tar.gz -o $HOME/root/tomcat.tar.gz &&
+tar xzf $HOME/root/tomcat.tar.gz -C $HOME/root/                                                                                &&
+mv $HOME/root/apache-tomcat-8.5.42 $HOME/root/.tomcat                                                                          &&
+rm -rf $HOME/root/tomcat.tar.gz                                                                                                &&
+find $HOME/root/.tomcat -maxdepth 1 -type f -delete                                                                            &&
+find $HOME/root/.tomcat/bin -maxdepth 1 -name "*.bat" -type f -delete                                                          &&
 rm -rf $HOME/root/.tomcat/webapps/docs $HOME/root/.tomcat/webapps/examples $HOME/root/.tomcat/webapps/host-manager
+
+docker run --name tomcat -d -i --privileged --restart always -p 8080:8080 -v $HOME/root/.tomcat:/root/.tomcat balajipothula/tomcat:openjdk8-jre

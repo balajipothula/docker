207a594cce27fc115a145c6e57e4b6b2def19067
Balaji Pothula <balan.pothula@gmail.com>
2023-02-05 18:35:30 +0100

    Update help-docker.sh

--- help-docker.sh
+++ help-docker.sh
@@ -53,3 +53,6 @@ docker start webapp
 
 # Stoping docker container with name.
 docker stop webapp
+
+# Running health check container.
+docker run --name health_checker -d -t --health-cmd "curl --fail 127.0.0.1" --health-interval=5s --health-retries=1 busybox sh
